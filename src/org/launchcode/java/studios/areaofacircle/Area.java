5836ae599fe4c454767f1d4b3ed73c828b3079bb
akyeshi <akyeshi@gmail.com>
2021-10-10 12:11:05 -0400

    Studio: Area of a Circle -> last bonus mission solved

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,17 +7,28 @@ public class Area {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius: ");
-        Double r = input.nextDouble();
-        String s = input.nextLine();
-        input.close();
+        Double r;
+        String s;
 
-        // adding validation to the program
+        /* adding validation to the program
         if (r < 0 && r.equals("") && (!s.matches("[0-9]+"))){
             System.out.println("invalid input! please enter a correct one");
         } else {
             Double area = Circle.getArea(r); // using another class file method here
             System.out.println("The area of a circle of radius " + r + " is: " + area);
         }
+        */
+
+        // bonus question: do-while validation
+        do {
+            r = input.nextDouble();
+            s = input.nextLine();
+            Double area = Circle.getArea(r); // using another class file method here
+            System.out.println("The area of a circle of radius " + r + " is: " + area);
+        } while (r < 0 && r.equals(""));
+
+        input.close();
+
     }
 
 }
