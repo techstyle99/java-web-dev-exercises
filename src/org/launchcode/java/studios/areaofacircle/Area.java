ef6932960bf688d218e082ae68c01bcd8dc2c330
akyeshi <akyeshi@gmail.com>
2021-10-10 12:01:40 -0400

    studio bonus question: added validation

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -6,11 +6,18 @@ import static java.lang.Math.*;
 public class Area {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("enter a radius number for the circle: ");
+        System.out.println("Enter a radius: ");
         Double r = input.nextDouble();
-        Double area = Circle.getArea(r);
-        System.out.println("The area of a circle of radius " + r + " is: " + area);
+        String s = input.nextLine();
+        input.close();
 
+        // adding validation to the program
+        if (r < 0 && r.equals("") && (!s.matches("[0-9]+"))){
+            System.out.println("invalid input! please enter a correct one");
+        } else {
+            Double area = Circle.getArea(r); // using another class file method here
+            System.out.println("The area of a circle of radius " + r + " is: " + area);
+        }
     }
 
 }
